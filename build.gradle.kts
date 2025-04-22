9fb14f2bf92c78188bc70d2d176f256489235ab2
JunGyeongCho <scope0204@gmail.com>
2025-04-22 22:51:50 +0900

    feat: Swagger 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
